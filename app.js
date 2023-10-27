8aef3ae0788af3acd1a412738ecba07c37a21eb9
anthonyhhhh <anthonyhidal733go@gmail.com>
2023-10-27 15:16:43 +0100

    ss

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+(async() => {
+    try {
+        await mongoose.connect(process.env.MONGOOSE_URI_TEST)
+        console.log('conectado a mongoose')
+    } catch (error) {
+        console.log()
+    }
+})();
+
+app.use('/', express.static(path.resolve('views', 'home')));
+app.use('/signup', express.static(path.resolve('views', 'signup')));
+app.use('/login', express.static(path.resolve('views', 'login')));
+
+
+
+module.exports = app;
